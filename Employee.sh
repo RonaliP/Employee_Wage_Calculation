1114f2e28eafa51850c8d6923f985984713c9112
Ronali <ronalipanigrahy88@gmail.com>
2020-10-18 13:03:06 +0530

    Reduced the code

--- Employee.sh
+++ Employee.sh
@@ -2,41 +2,30 @@
 
 echo "HELLO,WELCOME TO EMPLOYEE INFORMATION AND WAGE CALCULATION PROGRAM"
 
-#BELOW CODE WILL CHECK IF EMPLOYEE PRESENT OR NOT
-#UC1-Attendance_check
-
+working_hour=8
+Rate_hour=20
 check=$((RANDOM%2))
+
 if [ $check == 1 ]
 then
 	echo "EMPLOYEE IS PRESENT TODAY"
+	Dailywage=$((working_hour*Rate_hour))
+
 else
 	echo "EMPLOYEE IS ABSENT TODAY"
 fi
 
-echo "_____________________________________________"
-#BELOW CODE WILL CALCULATE DAILYWAGE IF EMPLOYEE PRESENT
-#UC2-Dailywage_calculation
-working_hour=8
-Rate_hour=20
-if [ $check == 1 ]
-then
-	Dailywage=$((working_hour*Rate_hour))
-	echo "Dailywage of employee per day "
-	echo "20 rs/- per hour*8 hours a day is $Dailywage"
-fi
-echo "______________________________________________"
 #BELOW CODE WILL ADD PART TIME WAGE CALCULATION FEATURE
-#UC3-Parttime_wage
+
+echo "____________________________________"
 parttime=$((RANDOM%2))
 if [ $parttime == 1 ]
 then
 	Partwage=$((working_hour*Rate_hour))
 	echo "Part time wage is $Partwage"
 fi
-echo "_____________________________________________"
-#BELOW CODE WILL CALCULATE WAGE USING SWITCH CASE
-#UC4-Switchcase
 
+echo "____________________________________"
 choice=$(((RANDOM%4)+1))
 
 case $choice in
@@ -49,70 +38,29 @@ case $choice in
 3) echo "EMPLOYEE IS ABSENT TODAY"
 	;;
 esac
-echo "_____________________________________________"
-#BELOW CODE WILL CALCULATE MONTHLY WAGE
-#UC5-Monthlywage_calculation
+
+echo "____________________________________"
 Monthlywage=0
+count=0
 for((i=1;i<=30;i++))
 do
     ispresent=$((RANDOM%2))
-#       echo $ispresent
     if [ $ispresent == 1 ]
     then
-             Monthlywage=$(( $Monthlywage + (20*8)))
+            count=$(($count+1))
+	    Monthlywage=$(( $Monthlywage + (20*8)))
      fi
 done
+echo "EMPLOYEE WAS PRESENT FOR $count DAYS IN MONTH"
 echo "EMPLOYEE'S MONTHLY WAGE BASED ON HIS ATTENDANCE=" $Monthlywage
-echo "______________________________________________"
-#BELOW CODE CALCULATES WAGE FOR 20 DAYS OR 100 HOURS
-#UC6-Conditionalcheck
 
-
-hour=1
-Maxday=20
-for((i=1;i<=$Maxday;i++))
-do
-    ispresent=$((RANDOM%2))
-
-    if [ $ispresent -eq 1 ]
-    then
-                hour=$(($hour+$hour))
-        fi
-done
-echo "TOTAL WORKING HOURS EMPLOYEE WORKED IN 20 DAYS=" $hour
-TotalWage=$((20*$hour))
-echo "TOTAL WAGE WILL BE GIVEN=" $TotalWage
-echo "____________________________________________________"
-#BELOW CODE WILL ADD FUNCTION TO THE ABOVE CONDITIONALCHECK SNIPPET
-#UC7-Function
-
+echo "_____________________________________"
+#USED FUNCTION AND STORED IN ARRAY AND DISPLAYED
 
 calculate()
 {
 Maxday=$1
 hour=$2
-for((i=1;i<=$Maxday;i++))
-do
-        ispresent=$((RANDOM%2))
-
-        if [ $ispresent -eq 1 ]
-        then
-                hour=$(($hour+$hour))
-        fi
-done
-echo "TOTAL WORKING HOURS EMPLOYEE WORKED IN 20 DAYS=" $hour
-TotalWage=$((20*$hour))
-echo "TOTAL WAGE WILL BE GIVEN=" $TotalWage
-}
-
-calculate 20 8
-echo "_________________________________________________"
-#BELOW CODE WILL DISPLAY STORED DAILY WAGES ALONG WITH TOTAL WAGES
-#UC8-StoreWages
-
-calculate()
-{
-Maxday=$1
 count=0
 TotalWage=0
 for((i=1;i<=$Maxday;i++))
@@ -121,16 +69,24 @@ do
 
         if [ $ispresent -eq 1 ]
         then
-		count=$((count+1))
+                count=$((count+1))
                 hour=$(($hour+$hour))
-		Perdaywage=$((Rate_hour*working_hour))
-		arr[$i]=$(("$Perdaywage"))
-                TotalWage=$(($TotalWage+$Perdaywage))
-	 echo "Day $count == ${arr[$i]} == $TotalWage"
-	fi
-done
+		if [ $hour -ge 100 ]
+		then
+			echo "Hours reached to maximum 100"
+			break
+		fi
 
+                Perdaywage=$((Rate_hour*working_hour))
+                arr[$i]=$(("$Perdaywage"))
+                TotalWage=$(($TotalWage+$Perdaywage))
+		echo "Day $count    ${arr[$i]}    $TotalWage"
+        fi
+
+done
+echo "TOTAL WORKING HOURS EMPLOYEE WORKED IN 20 DAYS=" $hour
+TotalWage=$((20*$hour))
+echo "TOTAL WAGE WILL BE GIVEN=" $TotalWage
 }
 
-calculate 20
-
+calculate 20 8
